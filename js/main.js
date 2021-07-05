cf24254156df6bb8b307d2592319666ff16805ff
ElizavetaG15 <lizaglevskaja@rambler.ru>
2021-07-05 13:56:04 +0300

    Стилизация радио-кнопок и правки на странице заказа

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ $(function() {
         $(this).find(".sort-dropdown-arr").toggleClass("caretup");
     });
 });
+
+$(function() {
+    $(".dropdown").on("show.bs.dropdown hide.bs.dropdown", function() {
+        $(this).find(".city-dropdown-arr").toggleClass("caretup");
+    });
+});
+
+$(function() {
+    $(".dropdown").on("show.bs.dropdown hide.bs.dropdown", function() {
+        $(this).find(".day-dropdown-arr").toggleClass("caretup");
+    });
+});
+
+
+$(function() {
+    $("#phone").mask("+7(999) 999-99-99");
+});
+
+
+
+$(function() {
+    $('.order-delete').on('click', function() {
+        $(this).parent().remove();
+    });
+});
